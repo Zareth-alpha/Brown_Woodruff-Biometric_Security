68960ff33bbbd7051c438e7e0c3639bb9683dbb6
Jonathan Brown <j.z.r.brown@asu.edu>
2020-05-02 16:12:06 -0700

    Add files via upload

--- findmyphone.py
+++ findmyphone.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Author: Jonathan Brown
 Date: 4/10/20
@@ -25,39 +26,33 @@ import bluetooth
 import getpass
 import os
 
-target_name = "My Phone"  # needs to prompt user or check some sort of settings...
+target_name = input("What is your device name: ")  # needs to prompt user or check some sort of settings...
 target_address = None
+# This needs to be used on the Android device's side.
+server_uuid = "a2087a8e-7dd3-11ea-bc55-0242ac130003"
 
 # The routine discover_devices() scans for approximately 10 seconds and returns a list of addresses of detected devices.
 nearby_devices = bluetooth.discover_devices()
 
-"""
-PyBluez represents a bluetooth address as a string of the form ``xx:xx:xx:xx:xx"
-"""
-
-# Check the available, nearby devices for a matching name. Assign it to target_address.
-for bdaddr in nearby_devices:
-    if target_name == bluetooth.lookup_name(bdaddr):
-        target_address = bdaddr
-        break
-
-# If there is an address, print it out.
-if target_address is not None:
-    print("found target bluetooth device with address %s \n" % target_address)
-# otherwise announce that there are none.
-else:
-    print("could not find target bluetooth device nearby.\n")
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
 # The documentation from the link above uses a depreciated function. This is the modern format.
 port = bluetooth.PORT_ANY
 server_sock.bind(("", port))
 server_sock.listen(1)
-# Debug print only. Comment out when testing is over.
-print("Listening on port: %d" % port)
+"""
+PyBluez represents a bluetooth address as a string of the form ``xx:xx:xx:xx:xx"
+"""
 
-# This needs to be used on the Android device's side.
-server_uuid = "a2087a8e-7dd3-11ea-bc55-0242ac130003"
+
+# FUNCTION DEFINITIONS:
+# Check the available, nearby devices for a matching name. return the target address.
+def lookup_device(target_device_name):
+    for bdaddr in nearby_devices:
+        if target_device_name == bluetooth.lookup_name(bdaddr):
+            target_device_address = bdaddr
+            return target_device_address
+
+
 bluetooth.advertise_service(server_sock, "Biometric Service", server_uuid)
 
 client_sock, address = server_sock.accept()
@@ -89,3 +84,15 @@ Error handling code has been omitted for clarity in the examples, but is fairly 
  operations fail for some reason (e.g. connection timeout, no local bluetooth resources are available, etc.) then a 
  BluetoothError is raised with an error message indicating the reason for failure. 
 """
+
+# Running Code here: - JB 5/2
+target_address = lookup_device(target_name)
+# If there is an address, print it out.
+if target_address is not None:
+    print("found target bluetooth device with address %s \n" % target_address)
+# otherwise announce that there are none.
+else:
+    print("could not find target bluetooth device nearby.\n")
+
+# Debug print only. Comment out when testing is over.
+print("Listening on port: %d" % port)
