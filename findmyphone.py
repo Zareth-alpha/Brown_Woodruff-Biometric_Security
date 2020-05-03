c293da71d66b076b2e933e99c51c840642f68da2
Jonathan Brown <j.z.r.brown@asu.edu>
2020-05-03 11:55:37 -0700

    Add files via upload

--- findmyphone.py
+++ findmyphone.py
@@ -21,45 +21,113 @@ File Description:
     libbluetooth-dev
     pip3 install pybluez
 """
-
+"""
 import bluetooth
 import getpass
 import os
 
+repeat = True
+while repeat:
+    hostMACAddress = input("What is your mobile device's MAC address: ")  # MAC address of Mobile Device.
+    print(hostMACAddress)
+    response = input("Correct?")
+    if response == 'y' or response == 'yes':
+        repeat = False
+port = 3
+backlog = 1
+size = 1024
+btSocket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+btSocket.bind((hostMACAddress, port))
+btSocket.listen(backlog)
+lock = 'loginctl lock-sessions'
+user = getpass.getuser()
+unlock = 'loginctl unlock-sessions {} --no-ask-password'.format(user)
+
+try:
+    client, clientInfo = btSocket.accept()
+    while True:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+            if data == 'lock':
+                os.system(lock)
+            elif data == 'unlock':
+                os.system(unlock)
+except:
+    print("Closing socket")
+    client.close()
+    btSocket.close()
+
+"""
+import bluetooth
+import getpass
+import os
+
+"""
 target_name = input("What is your device name: ")  # needs to prompt user or check some sort of settings...
 target_address = None
+"""
+backlog = 1
+size = 1024
 # This needs to be used on the Android device's side.
 server_uuid = "a2087a8e-7dd3-11ea-bc55-0242ac130003"
-
-# The routine discover_devices() scans for approximately 10 seconds and returns a list of addresses of detected devices.
-nearby_devices = bluetooth.discover_devices()
-
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-# The documentation from the link above uses a depreciated function. This is the modern format.
-port = bluetooth.PORT_ANY
-server_sock.bind(("", port))
-server_sock.listen(1)
-"""
-PyBluez represents a bluetooth address as a string of the form ``xx:xx:xx:xx:xx"
-"""
 
 
 # FUNCTION DEFINITIONS:
 # Check the available, nearby devices for a matching name. return the target address.
-def lookup_device(target_device_name):
-    for bdaddr in nearby_devices:
+def lookup_device(target_device_name, nearby_list):
+    print("lookup_device: " + target_device_name)
+    for bdaddr in nearby_list:
         if target_device_name == bluetooth.lookup_name(bdaddr):
+            print("Found target device: " + target_device_name + ": " + bdaddr)
             target_device_address = bdaddr
             return target_device_address
 
+"""
+attempts = 1
+target_name = input("Please enter your Android device name: ")
+while attempts < 5:
+    # The routine discover_devices() scans for approximately 10 seconds and returns a list of addresses of detected
+    # devices.
+    print("discover_devices: searching...")
+    nearby_devices = bluetooth.discover_devices()
 
-bluetooth.advertise_service(server_sock, "Biometric Service", server_uuid)
+    target_address = lookup_device(target_name, nearby_devices)
+    # If there is an address, print it out.
+    if target_address is not None:
+        print("found target bluetooth device with address %s \n" % target_address)
+    # otherwise announce that there are none.
+    elif attempts < 5:
+        attempts += 1
+        print("searching...")
+    else:
+        print("could not find target bluetooth device nearby.\n")
+"""
+
+print("server_sock: setting BluetoothSocket.")
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+print("server_sock: {}".format(str(server_sock)))
+
+# The documentation from the link above uses a depreciated function. This is the modern format.
+print("Setting any port.")
+port = bluetooth.PORT_ANY
+server_sock.bind(("", port))
+print("listening on port {}".format(port))
+
+print("server_sock: listening")
+server_sock.listen(backlog)
+
+bluetooth.advertise_service(server_sock, "Biometric Security", server_uuid)
 
 client_sock, address = server_sock.accept()
-print("Accepted connection from %s" % address)
+print("Accepted connection from {}".format(address))
 
-data = client_sock(1024)
-print("received [%s]" % data)
+data = client_sock(size)
+print("received {}".formet(data))
+'''
+PyBluez represents a bluetooth address as a string of the form ``xx:xx:xx:xx:xx"
+'''
 
 # ================ START OF TEST LINUX CODE ============================================================================
 
@@ -84,15 +152,3 @@ Error handling code has been omitted for clarity in the examples, but is fairly 
  operations fail for some reason (e.g. connection timeout, no local bluetooth resources are available, etc.) then a 
  BluetoothError is raised with an error message indicating the reason for failure. 
 """
-
-# Running Code here: - JB 5/2
-target_address = lookup_device(target_name)
-# If there is an address, print it out.
-if target_address is not None:
-    print("found target bluetooth device with address %s \n" % target_address)
-# otherwise announce that there are none.
-else:
-    print("could not find target bluetooth device nearby.\n")
-
-# Debug print only. Comment out when testing is over.
-print("Listening on port: %d" % port)
